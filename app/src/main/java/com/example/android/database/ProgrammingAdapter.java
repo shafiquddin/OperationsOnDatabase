95a3ef4e67a86815cc2e6465a6334a34fcda52e4
shafiquddin <shafiquddin2k@gmail.com>
2018-11-01 15:54:20 +0530

    delete

---
+++ app/src/main/java/com/example/android/database/ProgrammingAdapter.java
@@ -0,0 +1,46 @@
+//package com.example.android.database;
+//
+//import android.support.annotation.NonNull;
+//import android.support.v7.widget.RecyclerView;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//
+//public class ProgrammingAdapter extends RecyclerView.Adapter<ProgrammingAdapter.viewHolder> {
+//    String[] data;
+//
+//    public ProgrammingAdapter(String data) {
+//        this.data = data;
+//
+//    }
+//
+//    @NonNull
+//    @Override
+//    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+//        View view = inflater.inflate(R.layout.item_list, parent, false);
+//        return new viewHolder(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+//        String title = data[position];
+//        holder.txt.setText(title);
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return data.length;
+//    }
+//
+//    public class viewHolder extends RecyclerView.ViewHolder {
+//        TextView txt;
+//
+//        public viewHolder(View itemView) {
+//            super(itemView);
+//            txt = (TextView) itemView.findViewById(R.id.edit);
+//
+//        }
+//    }
+//}
